038e20660079965fd48e6549514e4c176936e32c
amadeuszkozlowski <amadeuszkozlowski@gmail.com>
2019-09-12 15:15:15 +0200

    [feature] home design

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <div id="app" class="flex min-h-screen flex-col overflow-x-hidden">
-    <router-view class="flex-1"/>
-  </div>
+    <div id="app" class="flex min-h-screen flex-col overflow-x-hidden bg-cover bg-center bg-no-repeat text-white"
+         :style="{backgroundImage: `url(${require('@/assets/images/bg.jpg')})`}">
+        <div class="gradient"></div>
+        <router-view class="flex-1 z-10"/>
+    </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,13 @@
 <template>
-    <div class="flex flex-col justify-center items-center">
-        <h1>Lorem ipsum dolor sit amet.</h1>
-        <h2>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus architecto atque excepturi iusto libero magni obcaecati sequi similique! Cumque debitis ex nostrum quae quidem quod recusandae rem repellendus sed vitae.</h2>
-        <router-link class="btn btn--green" to="calc" tag="button">Lorem ipsum dolor!</router-link>
+    <div class="flex">
+        <div class="container mx-auto flex flex-col justify-center items-center">
+            <h1 class="text-5xl">Lorem ipsum dolor sit amet.</h1>
+            <h2 class="text-2xl text-center mb-12 max-w-5xl">
+                Lorem ipsum dolor <strong>sit amet, consectetur adipisicing</strong>
+                elit. Accusamus architecto atque excepturi iusto libero magni obcaecati sequi similique! Cumque debitis ex nostrum <strong>quae quidem quod recusandae</strong>
+                rem repellendus sed vitae. Lorem ipsum dolor sit amet.</h2>
+            <router-link class="btn btn--gradient" to="calc" tag="button">Lorem ipsum dolor!</router-link>
+        </div>
     </div>
 </template>
 

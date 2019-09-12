4672a08f45b349b3f52f08b162fa07c53f2ce43d
amadeuszkozlowski <amadeuszkozlowski@gmail.com>
2019-09-12 13:38:27 +0200

    [feature] init project, vue, routing, tailwind

---
+++ src/views/Calc.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="flex flex-col justify-center items-center">
+        Calc
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'calc',
+  };
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,14 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="flex flex-col justify-center items-center">
+        <h1>Lorem ipsum dolor sit amet.</h1>
+        <h2>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus architecto atque excepturi iusto libero magni obcaecati sequi similique! Cumque debitis ex nostrum quae quidem quod recusandae rem repellendus sed vitae.</h2>
+        <router-link class="btn btn--green" to="calc" tag="button">Lorem ipsum dolor!</router-link>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld,
-  },
-};
+  export default {
+    name: 'home',
+  };
 </script>
